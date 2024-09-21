d58dc380bc1a79113b41002d7dd3b5aa6936f263
[your <tw841117@gmail.com>
2024-09-20 23:58:21 -0400

    upload hello_fastapi.py

--- hello_fastapi.py
+++ hello_fastapi.py
@@ -1,5 +1,7 @@
 # use unicorn to run
-# uvicorn hello_fastapi:app reload
+# uvicorn hello_fastapi:app --reload
+
+import uvicorn
 
 from fastapi import FastAPI
 
@@ -12,3 +14,8 @@ async def hello_fastapi():
 @app.get('/hellofastapi')
 async def hello_fastapi():
     return {'message': "Hello FastAPI from ./hellofastapi!"}
+
+if __name__ == "__main__":
+
+    # equal to uvicorn hello_fastapi:app --reload
+    uvicorn.run("helloworld:app", reload=True)
